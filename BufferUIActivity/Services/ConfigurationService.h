f83ef75afd0a4ed40a7b2301c6bfcd2c8af529a2
Andrew Yates <contact@andydev.co.uk>
2012-10-08 13:22:20 +0100

    Add configuration service

---
+++ BufferUIActivity/Services/ConfigurationService.h
@@ -0,0 +1,15 @@
+//
+//  ConfigurationService.h
+//  Buffer
+//
+//  Created by Andrew Yates on 04/10/2012.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ConfigurationService : NSObject
+
+-(void)getConfigurationWithSender:(id)sender;
+
+@end
